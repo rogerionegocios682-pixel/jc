5c5343ca791bd1bfd399083c3f0f36f093e24860
rogerionegocios682-pixel <rogerionegocios682@gmail.com>
2026-07-01 02:16:17 -0300

    Replace placeholder Firebase config with real project credentials

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,14 @@ import { getDatabase, ref, set, get, onValue, push, remove, update } from "https
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
+  apiKey: "AIzaSyBP1creZJ_YwNKGNq8yI3Hr3MffE4smPl4",
   authDomain: "jcempreendimentos-ea208.firebaseapp.com",
   databaseURL: "https://jcempreendimentos-ea208-default-rtdb.firebaseio.com",
   projectId: "jcempreendimentos-ea208",
-  storageBucket: "jcempreendimentos-ea208.appspot.com",
-  messagingSenderId: "..."
+  storageBucket: "jcempreendimentos-ea208.firebasestorage.app",
+  messagingSenderId: "403582280551",
+  appId: "1:403582280551:web:d27bc852d4684b713785c2",
+  measurementId: "G-2J5TSC3HEC"
 };
 
 // Inicializar o Firebase
